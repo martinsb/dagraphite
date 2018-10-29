e0e5bbd31b02aa365f3405f62e3ff17f88120fa5
Mārtiņš Barinskis <martins@barinskis.me>
2018-10-29 19:46:08 +0200

    save and load images to server

---
+++ server/server.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
+const createStorage = require('./storage');
+const slowerMiddleware = require('./slower');
+
+const app = express();
+const jsonParser = bodyParser.json();
+
+//artificially slows down server response time from 1 to 10 sec.
+app.use(slowerMiddleware(1000, 10000));
+
+app.use(function(req, res, next) {
+    jsonParser(req, res, function(err) {
+        if (err instanceof SyntaxError) {
+            res.status(400).send('Invalid JSON syntax');
+            return;
+        }
+        next.apply(null, arguments);
+    });
+});
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+const uploadDir = path.resolve(path.join(__dirname, '../public/tmp/'));
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
+const upload = multer({dest: os.tmpdir()});
+
+const httpPort = 8080;
+
+let storage;
+(async function() {
+    try {
+        storage = await createStorage(path.join(__dirname, 'dagraphite.db'));
+    } catch (e) {
+        console.error('Could not create storage', e);
+        process.exit(1);
+    }
+})();
+
+app.get('/api/images', async function(req, res, next) {
+    try {
+        const images = await storage.getAllImages();
+        res.status(200).json(images.map(({id, file, description}) => ({
+            id,
+            url: '/' + file,
+            description
+        })));
+    } catch (e) {
+        console.error('Could not fetch images', e);
+        res.status(500).send('Internal server error');
+    }
+});
+
+app.post('/api/images', upload.single('image'), async function(req, res, next) {
+    const {id} = req.body;
+    if (!id) {
+        res.status(400).send('No image ID is provided');
+        return;
+    }
+    let destPath = path.join(uploadDir, req.file.originalname.replace(/\s+/, '_'));
+    if (fs.existsSync(destPath)) {
+        //Since a file with such name already exists in our FS,
+        //let's find another suitable name for the uploaded file.
+        //We also do not limit iteration count for the sake of simplicity.
+        let iteration = 0;
+        const filenameParts = req.file.originalname.split('.');
+        do {
+            iteration++;
+            const filenameToTest = [`${filenameParts[0]}_(${iteration})`, ...filenameParts.slice(1)].join('.');
+            destPath = path.join(uploadDir, filenameToTest);
+        } while (fs.existsSync(destPath));
+    }
+    fs.copyFileSync(req.file.path, destPath);
+    const urlPath = path.relative(path.resolve(path.join(__dirname, '../public')), destPath);
+    try {
+        await storage.saveImage({id, file: urlPath});
+        res.status(201).end();
+    } catch (e) {
+        res.status(500).send('Internal server error, something went wrong');
+        return;
+    }
+});
+
+app.listen(httpPort, function() {
+    console.log(`App started and listening to port ${httpPort}`);
+});
---
+++ server/storage.js
@@ -0,0 +1,69 @@
+const util = require('util');
+const assert = require('assert');
+
+const sqlite3 = require('sqlite3');
+
+class Storage {
+    constructor(db) {
+        this._db = db;
+    }
+
+    saveImage({id, file, thumbnail, description}) {
+        return this._db.runAsync('INSERT INTO images (id, file_path, description) VALUES (?, ?, ?)', [id, file, description]);
+    }
+
+    async getImage(id) {
+        const row = await this._db.getAsync('SELECT id, file_path, description FROM images WHERE id=?', [id]);
+        mapImageRow(row);
+    }
+
+    async getAllImages() {
+        const rows = await this._db.allAsync('SELECT id, file_path, description FROM images');
+        return rows.map(mapImageRow);
+    }
+}
+
+const mapImageRow = ({id, file_path, description}) => ({id, file: file_path, description});
+
+function createStorage(filePath) {
+    return new Promise((resolve, reject) => {
+        const mode = sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE;
+        const db = new sqlite3.Database(filePath, mode, async (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                promisifyObject(db);
+                try {
+                    await ensureTableSchema(db);
+                    resolve(new Storage(db));
+                } catch (e) {
+                    reject(e);
+                    return;
+                }
+            }
+        });
+    });
+}
+
+function promisifyObject(obj) {
+    for (const fieldName in obj) {
+        const field = obj[fieldName];
+        if (typeof field === 'function') {
+            const asyncFieldName = fieldName + 'Async';
+            assert(!(asyncFieldName in obj), `Field ${asyncFieldName} already exists in the object`);
+            obj[asyncFieldName] = util.promisify(field.bind(obj));
+        }
+    }
+    return obj;
+}
+
+async function ensureTableSchema(db) {
+    await db.runAsync(`CREATE TABLE IF NOT EXISTS images (
+                           id TEXT NOT NULL PRIMARY KEY,
+                           file_path TEXT NOT NULL UNIQUE,
+                           thumbnail_path TEXT NOT NULL UNIQUE,
+                           description TEXT
+    )`);
+}
+
+module.exports = createStorage;
